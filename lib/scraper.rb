84d3c948deb972ebceec092a3df3175313828a6a
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-30 23:55:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+#puts "#{doc}"
+
+puts doc.css(".headline-260IBN").text
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
